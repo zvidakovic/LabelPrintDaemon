5bb4d21d8b4e1132b43b277a932a4d64cd2ab61e
Zoran Vidakovic <zvidakov@gmail.com>
2018-03-05 19:52:50 +0100

    init

--- LabelPrintDaemon/Program.vb
+++ LabelPrintDaemon/Program.vb
@@ -2,45 +2,68 @@ Imports System.Net
 Imports System.IO
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("PROGRAM STARTED")
+    Dim logPath As String
+    Dim logWriter As StreamWriter
 
-        Console.WriteLine("INITIALIZING VARIABLES...")
+    Sub Main(args As String())
+        logPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\LabelPrintDaemon-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".log"
+        logWriter = New StreamWriter(logPath)
+
+        Log("Program started.")
+        If args.Count < 1 Then
+            Log("Missing argument. Please, provide absolute path to LBX file. Exiting.")
+            Return
+        End If
+
+        Dim templatePath As String = args(0).Trim()
+
+        If File.Exists(templatePath) = False Then
+            Log("File " + templatePath + " does not exists. Exiting.")
+            Return
+        End If
+
+        Log("LBX file path is " + args(0))
+        Log("Log file path is " + logPath)
+
         Dim dataUrl As String = "https://jsonplaceholder.typicode.com/posts"
-        Dim templatePath As String = "C:\LabelPrintDaemon.lbx"
         Dim doc = New bpac.Document
         Dim webRequest As WebRequest
         Dim stream As Stream
-        Console.WriteLine("INITIALIZING VARIABLES COMPLETED")
 
+        Log("Doing GET request on " + dataUrl)
         webRequest = WebRequest.Create(dataUrl)
-
-        Console.WriteLine("DOING GET REQUEST ON URL: " + dataUrl)
-
         stream = webRequest.GetResponse.GetResponseStream()
 
         Dim reader As New StreamReader(stream)
         Dim sLine As String = ""
         Dim i As Integer = 0
-
         Do While Not sLine Is Nothing
             i += 1
             sLine = reader.ReadLine
             If Not sLine Is Nothing Then
-                Console.WriteLine("{0}:{1}", i, sLine)
+                'Console.WriteLine("{0}:{1}", i, sLine)
             End If
         Loop
 
-        Console.ReadLine()
-
-        Console.WriteLine("STARTING TO PRINT")
+        Log("Starting to print...")
         doc.Open(templatePath)
         doc.StartPrint("", bpac.PrintOptionConstants.bpoDefault)
         doc.PrintOut(2, bpac.PrintOptionConstants.bpoDefault)
         doc.EndPrint()
         doc.Close()
-        Console.WriteLine("PRINT DONE")
+        Log("Printing done.")
 
-        Console.WriteLine("PROGRAM IS DONE. EXITING.")
+        Log("Program is done. Exiting.")
     End Sub
+
+    Function Log(ByVal message As String) As Boolean
+        If logWriter Is Nothing Then
+            Return False
+        End If
+        logWriter.WriteLine(message)
+        logWriter.Flush()
+        Console.WriteLine(message)
+        Return True
+    End Function
+
 End Module
